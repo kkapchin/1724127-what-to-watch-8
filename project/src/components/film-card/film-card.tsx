b7ca9771a2bd02d5174f62b7cddcf78e016bff7f
kkapchin <kkapchin@yandex.ru>
2021-11-17 20:20:46 +0300

    Video Player

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,20 +1,32 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import { Film } from '../../types/films';
+import VideoPlayer from '../video-player/video-player';
 
 type FilmCardProps = {
   film: Film,
 }
 
 function FilmCard({film}: FilmCardProps): JSX.Element {
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
   const url = `/films/${film.id}`;
+
   return (
     <article className="small-film-card catalog__films-card">
-      <div className="small-film-card__image">
-        <img src={film.previewImage} alt={film.name} width="280" height="175" />
-      </div>
-      <h3 className="small-film-card__title">
-        <Link className="small-film-card__link" to={url}>{film.name}</Link>
-      </h3>
+      <VideoPlayer
+        src={film.videoLink}
+        poster={film.posterImage}
+        name={film.name}
+        url={url}
+        isPlaying={isPlaying}
+        onMouseEnter={() => {
+          setIsPlaying((prevIsPlaying) => !prevIsPlaying);
+        }}
+        onMouseLeave={() => {
+          setIsPlaying((prevIsPlaying) => !prevIsPlaying);
+        }}
+      />
     </article>
   );
 }
